03b7630812d809df446a6542935b6a8d6288b5ba
Arshad Ullah <mdarshadullah835@gmail.com>
2022-10-11 20:59:09 +0600

    not found page is comoplated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Topics from './Components/Topics/Topics';
 import Main from './Layout/Main';
 import Statistics from './Layout/Statistics';
 import Blog from './Components/Blog/Blog';
+import Error from './Components/Error/Error';
 
 
 
@@ -20,10 +21,11 @@ function App() {
           }, element: <Topics></Topics>
         },
         { path: '/statistics', element: <Statistics></Statistics> },
-        { path: '/blog', element: <Blog></Blog> }
+        { path: '/blog', element: <Blog></Blog> },
       ],
       element: <Main></Main>
     },
+    { path: '*', element: <Error></Error> }
   ])
 
 
---
+++ src/Components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='grid justify-center'>
+            <h1 className=' rounded-lg text-center  font-semibold text-4xl mt-9 bg-teal-800 p-20 text-white'>Page is Not Found 404!Please Try Agin</h1>
+        </div>
+    );
+};
+
+export default Error;
