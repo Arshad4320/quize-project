e082e42028c7248d34a175053a93c336076d3650
Arshad Ullah <mdarshadullah835@gmail.com>
2022-10-12 20:44:53 +0600

    update to eye button

--- src/Components/QuizData/QuizData.js
+++ src/Components/QuizData/QuizData.js
@@ -1,20 +1,14 @@
+import { Button } from '@mui/material';
 import React from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { BeakerIcon, EyeIcon } from '@heroicons/react/24/solid'
 import OPtions from '../Options/OPtions';
+import { Swal } from 'sweetalert2/dist/sweetalert2.js';
+
 
 const QuizData = ({ quiz }) => {
     const { question, options, correctAnswer } = quiz;
-    // const handleQuiz = option => {
-    //     if (option === correctAnswer) {
-    //         alert('your ans is right')
-    //     }
-    //     else {
-    //         alert('your ans is wrong')
-    //     }
-    // }
-
     const handleQuiz = (option) => {
         if (option === correctAnswer) {
             toast.success("Congress!Right Ans.", {
@@ -28,16 +22,21 @@ const QuizData = ({ quiz }) => {
             });
         }
     }
-
-
-
-
-
-
+    const handleClickEyeButton = () => {
+        toast.success(correctAnswer, {
+            position: "top-center",
+            autoClose: 2000,
+        });
+    }
     return (
         <div>
-            <div className='bg-teal-700 mx-10 mb-4 rounded-lg '>
+            <div className='bg-teal-700 mx-10 mb-4 rounded-lg  '>
                 <p className='bg-teal-900 text-white  rounded p-3 text-left text-2xl text-semibold' >{question}</p>
+                <div>
+                    <Button onClick={handleClickEyeButton}>
+                        <EyeIcon className="h-6 w-6 text-gray-200 " />
+                    </Button>
+                </div>
                 {
                     options.map(options => <OPtions handleQuiz={handleQuiz} quiz={quiz} key={quiz.id} option={options}></OPtions>)
                 }
--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, } from 'react-router-dom';
-
-
+// import { useState } from "react";
 const Header = () => {
-
+    const [navbar, setNavbar] = useState(false);
     return (
-        <div>
-            <div className='grid grid-cols-2 bg-teal-500'>
-                <div className="lugo">
-                    <h2 className='font-extrabold mt-3   md:text-2xl sm:text-xl lg:text-3xl text-red-900'>WebTesty</h2>
+        <nav className="w-full  shadow bg-orange-200 ">
+            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        <h2 className='font-extrabold   md:text-2xl sm:text-xl lg:text-3xl text-red-900'>WebTesty</h2>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className='text-xl p-4 font-semibold text-red-900'>
-                    <Link className='p-6 font-2xl' to="/topics">Topics</Link>
-                    <Link className='p-6 font-2xl' to="/statistics">Statistics</Link>
-                    <Link className='p-6 font-2xl' to="/blog">Blog</Link>
+                <div>
+                    <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"}`}>
+                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            <li className="text-xl p-4 font-semibold text-red-900 hover:text-blue-900">
+                                <Link className='p-6 font-2xl` ' to="/topics">Topics</Link>
+                            </li>
+                            <li className="text-xl p-4 font-semibold text-red-900 hover:text-blue-900">
+                                <Link className='p-6 font-2xl` ' to="/statistics">Statistics</Link>
+                            </li>
+                            <li className="text-xl p-4 font-semibold text-red-900 hover:text-blue-900">
+                                <Link className='p-6 font-2xl` ' to="/blog">Blog</Link>
+                            </li>
+
+                        </ul>
+                    </div>
                 </div>
             </div>
-
-
-        </div>
+        </nav>
     );
-};
-
+}
 export default Header;
+
+
